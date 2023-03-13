404eb4d5385ebb70daf5793cd98f43889d5b49f8
mislavwinkler <mislav.winkler@gmail.com>
2023-03-13 22:15:22 +0100

    Initial commit

---
+++ src/main/java/hr/tvz/TameBackendApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TameBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/tvz/TameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TameBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
