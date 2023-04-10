74a4dd95fa8619db896c90c068245acf1e28a256
mislavwinkler <mislav.winkler@gmail.com>
2023-04-10 21:48:35 +0200

    add tests

--- src/test/java/hr/tvz/pios/tame/CommentControllerTest.java
+++ src/test/java/hr/tvz/pios/tame/CommentControllerTest.java
@@ -1,4 +1,84 @@
 package hr.tvz.pios.tame;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class CommentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getAllPost() throws Exception {
+        this.mockMvc.perform(
+                        get("/comment")
+                                .with(user("admin")
+                                        .password("admin")
+                                        .roles("ADMIN")
+                                )
+                                .with(csrf())
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+
+    }
+
+    @Test
+    void getCommentsByPostId() throws Exception {
+        this.mockMvc.perform(
+                        get("/comment/1")
+                                .with(user("admin")
+                                        .password("admin")
+                                        .roles("ADMIN")
+                                )
+                                .with(csrf())
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+
+    }
+
+    @Test
+    @DirtiesContext
+    void saveComment() throws Exception {
+        String requestBody = "{\"text\":\"Test\", \"postId\":1, \"makerUsername\":\"admin\"}";
+        this.mockMvc.perform(
+                        post("/comment")
+                                .with(user("admin")
+                                        .password("admin")
+                                        .roles("ADMIN")
+                                )
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody)
+                )
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+
+    @Test
+    @DirtiesContext
+    void deleteComment() throws Exception {
+        this.mockMvc.perform(
+                        delete("/comment/1")
+                                .with(user("admin")
+                                        .password("admin")
+                                        .roles("ADMIN")
+                                )
+                                .with(csrf())
+                )
+                .andExpect(status().isNoContent());
+    }
 }
--- src/test/java/hr/tvz/pios/tame/PostControllerTest.java
+++ src/test/java/hr/tvz/pios/tame/PostControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -61,5 +62,51 @@ public class PostControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(content().string("[]"));
+    }
+    @Test
+    void getPostById() throws Exception {
+        this.mockMvc.perform(
+                        get("/post/1")
+                                .with(user("admin")
+                                        .password("admin")
+                                        .roles("ADMIN")
+                                )
+                                .with(csrf())
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.id").value(1));
+    }
+
+    @Test
+    @DirtiesContext
+    void savePost() throws Exception {
+        String requestBody = "{\"text\":\"Test\", \"makerUsername\":\"admin\"}";
+        this.mockMvc.perform(
+                        post("/post")
+                                .with(user("admin")
+                                        .password("admin")
+                                        .roles("ADMIN")
+                                )
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody)
+                )
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+
+    @Test
+    @DirtiesContext
+    void deletePost() throws Exception {
+        this.mockMvc.perform(
+                        delete("/post/1")
+                                .with(user("admin")
+                                        .password("admin")
+                                        .roles("ADMIN")
+                                )
+                                .with(csrf())
+                )
+                .andExpect(status().isNoContent());
     }
 }
